9b94f444cbc48bb87449171eb0412ea6baad91a0
Nick <nhranitz@gmail.com>
2018-11-07 21:33:02 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/nhran/whatsit/AuthenticatorActivity.kt
@@ -0,0 +1,41 @@
+package com.example.nhran.whatsit
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_authenticator.*
+import android.os.AsyncTask.execute
+import com.amazonaws.mobile.auth.ui.SignInUI
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.client.AWSStartupResult
+import com.amazonaws.mobile.client.AWSStartupHandler
+
+
+
+class AuthenticatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authenticator)
+        setSupportActionBar(toolbar)
+        // Add a call to initialize AWSMobileClient
+        AWSMobileClient.getInstance().initialize(this) {
+            val signin = AWSMobileClient.getInstance().getClient(
+                this@AuthenticatorActivity,
+                SignInUI::class.java
+            ) as SignInUI
+            signin.login(
+                this@AuthenticatorActivity,
+                HomeActivity::class.java
+            ).execute()
+        }.execute()
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
