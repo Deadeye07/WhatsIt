5e19e3c0cc48ebfa13e4596014d38e4e6eed82c3
Nick <nhranitz@gmail.com>
2018-11-11 21:34:22 -0500

    banner ad

--- app/src/main/java/com/example/nhran/whatsit/HomeActivity.kt
+++ app/src/main/java/com/example/nhran/whatsit/HomeActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -12,6 +15,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        MobileAds.initialize(this, "ca-app-pub-2805994884844744~5962103159");
+
+        val mAdView = findViewById<AdView>(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 
 }
