1b9da8c26bd9c156898c701d313896ee4a19de27
Nick <nhranitz@gmail.com>
2018-12-17 22:39:29 -0500

    call and receive ebay search data from aws

--- app/src/main/java/com/example/nhran/whatsit/SearchActivity.kt
+++ app/src/main/java/com/example/nhran/whatsit/SearchActivity.kt
@@ -53,7 +53,7 @@ class SearchActivity : BaseActivity() {
         editText.setText(searchTerm)
 
         //Do ebay search
-        doSearch(searchTerm)
+        //doSearch(searchTerm)
 
 
         //Set on click listener on tile to go to platform detail activity
@@ -94,6 +94,7 @@ class SearchActivity : BaseActivity() {
                 if (responseContentStream != null) {
                     val responseData = IOUtils.toString(responseContentStream)
                     // Do something with the response data here
+                    doSearch(responseData)
                     Log.d(TAG, responseData)
                 }
             } catch (ex: Exception) {
@@ -102,7 +103,45 @@ class SearchActivity : BaseActivity() {
         }
 
     }
-     private fun ebayTileClicked(){
+
+    private fun doSearch(response: String) {
+        //Create graph
+        val responseJSON = JSONObject(response)
+
+        val graphJSONData = responseJSON.getJSONArray("graph")
+
+        val groupOne = graphJSONData.getJSONObject(0).getString("group")
+        val graph = findViewById<View>(R.id.miniGraph) as GraphView
+        // use static labels for horizontal and vertical labels
+        val staticLabelsFormatter = StaticLabelsFormatter(graph)
+        staticLabelsFormatter.setHorizontalLabels(arrayOf(graphJSONData.getJSONObject(0).getString("group"), graphJSONData.getJSONObject(1).getString("group"),graphJSONData.getJSONObject(2).getString("group"),graphJSONData.getJSONObject(3).getString("group"), graphJSONData.getJSONObject(4).getString("group")))
+        graph.gridLabelRenderer.labelFormatter = staticLabelsFormatter
+        val series = BarGraphSeries<DataPoint>(
+            arrayOf(
+                DataPoint(0.0, graphJSONData.getJSONObject(0).getString("count").toDouble()),
+                DataPoint(1.0, graphJSONData.getJSONObject(1).getString("count").toDouble()),
+                DataPoint(2.0, graphJSONData.getJSONObject(2).getString("count").toDouble()),
+                DataPoint(3.0, graphJSONData.getJSONObject(3).getString("count").toDouble()),
+                DataPoint(4.0, graphJSONData.getJSONObject(4).getString("count").toDouble())
+
+            )
+        )
+        graph.addSeries(series)
+        //GEt the listview to populate
+        val averageTextField = findViewById<TextView>(R.id.ebayAverage)
+        val highTextField =findViewById<TextView>(R.id.highPrice)
+        val lowTextField =findViewById<TextView>(R.id.lowPrice)
+
+        val concatAverage = getString(R.string.dollar_sign) + responseJSON.getString("average")
+        val concatHigh = getString(R.string.dollar_sign) + responseJSON.getString("max")
+        val concatLow = getString(R.string.dollar_sign) + responseJSON.getString("min")
+
+        averageTextField.text = concatAverage
+        highTextField.text = concatHigh
+        lowTextField.text = concatLow
+    }
+
+    private fun ebayTileClicked(){
         //Go to search activity and pass in search term
         val intent = Intent(this, EbayResults::class.java).apply {
             //putExtra(EXTRA_MESSAGE, searchTerm)
@@ -115,105 +154,79 @@ class SearchActivity : BaseActivity() {
         return true
     }
 
-    private fun doSearch(term:String){
-        val builder = Uri.Builder()
-        builder.scheme("http")
-            .authority("svcs.ebay.com")
-            .appendPath("services")
-            .appendPath("search")
-            .appendPath("FindingService")
-            .appendPath("v1")
-            .appendQueryParameter("keywords", term.toString())
-            .appendQueryParameter("OPERATION-NAME", "findItemsByKeywords")
-            .appendQueryParameter("SERVICE-VERSION", "1.0.0")
-            .appendQueryParameter("SECURITY-APPNAME", "Nicholas-WhatsIt-PRD-07f960b4b-63e9548e")
-            .appendQueryParameter("GLOBAL-ID", "EBAY-US")
-            .appendQueryParameter("RESPONSE-DATA-FORMAT", "JSON")
-
-        val myUrl = builder.build().toString()
-
-// Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-
-// Request a string response from the provided URL.
-        val stringRequest = StringRequest(
-            Request.Method.GET, myUrl,
-            Response.Listener<String> { response ->
-                //Take JSON and put into an array
-                val myItems =arrayListOf<String>()
-
-                val jObject = JSONObject(response)
-
-                val test = jObject.getJSONArray("findItemsByKeywordsResponse").getJSONObject(0)
-
-                val searchResult = test.getJSONArray("searchResult").getJSONObject(0)
-
-                val items = searchResult.getJSONArray("item")
-
-                val allPrices = arrayListOf<Int>()
-
-                for (i in 0 until items.length()) {
-                    val item = items.getJSONObject(i)
-                    val sellingStatus = item.getJSONArray("sellingStatus")
-                    val price = sellingStatus.getJSONObject(0)
-                    val priceArray = price.getJSONArray("currentPrice")
-                    val priceGetingCloser = priceArray.getJSONObject(0)
-                    val finalPrice = priceGetingCloser.getInt("__value__")
-                    //val name = item.getJSONArray("name").toInt()
-                    allPrices.add(finalPrice)
-                    //myItems.add(title.toString())
-                }
-                val averagePrice = allPrices.average().toString()
-                val lowestPrice  = allPrices.min().toString()
-                val highestPrice = allPrices.max().toString()
-                //Create array adapter
-                //val newAdapter =  ArrayAdapter<String>(this, R.layout.platform_results, averagePrice)
-
-                val rangeIncrement = (highestPrice.toInt().minus(lowestPrice.toInt())) / 3 //Create 3 price groups that will be bars
-
-                val firstIncrement = lowestPrice.toInt() + rangeIncrement
-                val secondIncrement = firstIncrement + rangeIncrement
-                val thirdIncrement = secondIncrement + rangeIncrement
-                //loop through prices, if it's less than increment, then add to that increments count and remove it from price array
-
-                val firstIncrementCount = allPrices.count { it < firstIncrement }
-                val secondIncrementCount = allPrices.count { it in firstIncrement..secondIncrement}
-                val thirdIncrementCount = allPrices.count { it in secondIncrement..thirdIncrement}
-
-                //Create graph
-                val graph = findViewById<View>(R.id.miniGraph) as GraphView
-                // use static labels for horizontal and vertical labels
-                val staticLabelsFormatter = StaticLabelsFormatter(graph)
-                staticLabelsFormatter.setHorizontalLabels(arrayOf(lowestPrice + '-'+ firstIncrement.toString(),firstIncrement.toString()+'-'+secondIncrement ,secondIncrement.toString() + '-' +thirdIncrement ))
-                graph.gridLabelRenderer.labelFormatter = staticLabelsFormatter
-                val series = BarGraphSeries<DataPoint>(
-                    arrayOf(
-                        DataPoint(0.0, firstIncrementCount.toDouble()),
-                        DataPoint(1.0, secondIncrementCount.toDouble()),
-                        DataPoint(2.0, thirdIncrementCount.toDouble())
-
-                    )
-                )
-                graph.addSeries(series)
-                //GEt the listview to populate
-                val averageTextField = findViewById<TextView>(R.id.ebayAverage)
-                val highTextField =findViewById<TextView>(R.id.highPrice)
-                val lowTextField =findViewById<TextView>(R.id.lowPrice)
-
-                val concatAverage = getString(R.string.dollar_sign) + averagePrice
-                val concatHigh = getString(R.string.dollar_sign) + highestPrice
-                val concatLow = getString(R.string.dollar_sign) + lowestPrice
-
-                averageTextField.text = concatAverage
-                highTextField.text = concatHigh
-                lowTextField.text = concatLow
-
-            },
-            Response.ErrorListener {
-                Snackbar.make(findViewById(R.id.activitySearch),R.string.error, 5000).show()
-            })
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest)
-    }
+//     fun doSearch(term:String){
+//        val builder = Uri.Builder()
+//        builder.scheme("http")
+//            .authority("svcs.ebay.com")
+//            .appendPath("services")
+//            .appendPath("search")
+//            .appendPath("FindingService")
+//            .appendPath("v1")
+//            .appendQueryParameter("keywords", term.toString())
+//            .appendQueryParameter("OPERATION-NAME", "findItemsByKeywords")
+//            .appendQueryParameter("SERVICE-VERSION", "1.0.0")
+//            .appendQueryParameter("SECURITY-APPNAME", "Nicholas-WhatsIt-PRD-07f960b4b-63e9548e")
+//            .appendQueryParameter("GLOBAL-ID", "EBAY-US")
+//            .appendQueryParameter("RESPONSE-DATA-FORMAT", "JSON")
+//
+//        val myUrl = builder.build().toString()
+//
+//// Instantiate the RequestQueue.
+//        val queue = Volley.newRequestQueue(this)
+//
+//// Request a string response from the provided URL.
+//        val stringRequest = StringRequest(
+//            Request.Method.GET, myUrl,
+//            Response.Listener<String> { response ->
+//                //Take JSON and put into an array
+//                val myItems =arrayListOf<String>()
+//
+//                val jObject = JSONObject(response)
+//
+//                val test = jObject.getJSONArray("findItemsByKeywordsResponse").getJSONObject(0)
+//
+//                val searchResult = test.getJSONArray("searchResult").getJSONObject(0)
+//
+//                val items = searchResult.getJSONArray("item")
+//
+//                val allPrices = arrayListOf<Int>()
+//
+//                for (i in 0 until items.length()) {
+//                    val item = items.getJSONObject(i)
+//                    val sellingStatus = item.getJSONArray("sellingStatus")
+//                    val price = sellingStatus.getJSONObject(0)
+//                    val priceArray = price.getJSONArray("currentPrice")
+//                    val priceGetingCloser = priceArray.getJSONObject(0)
+//                    val finalPrice = priceGetingCloser.getInt("__value__")
+//                    //val name = item.getJSONArray("name").toInt()
+//                    allPrices.add(finalPrice)
+//                    //myItems.add(title.toString())
+//                }
+//                val averagePrice = allPrices.average().toString()
+//                val lowestPrice  = allPrices.min().toString()
+//                val highestPrice = allPrices.max().toString()
+//                //Create array adapter
+//                //val newAdapter =  ArrayAdapter<String>(this, R.layout.platform_results, averagePrice)
+//
+//                val rangeIncrement = (highestPrice.toInt().minus(lowestPrice.toInt())) / 3 //Create 3 price groups that will be bars
+//
+//                val firstIncrement = lowestPrice.toInt() + rangeIncrement
+//                val secondIncrement = firstIncrement + rangeIncrement
+//                val thirdIncrement = secondIncrement + rangeIncrement
+//                //loop through prices, if it's less than increment, then add to that increments count and remove it from price array
+//
+//                val firstIncrementCount = allPrices.count { it < firstIncrement }
+//                val secondIncrementCount = allPrices.count { it in firstIncrement..secondIncrement}
+//                val thirdIncrementCount = allPrices.count { it in secondIncrement..thirdIncrement}
+//
+//
+//
+//            },
+//            Response.ErrorListener {
+//                Snackbar.make(findViewById(R.id.activitySearch),R.string.error, 5000).show()
+//            })
+//
+//// Add the request to the RequestQueue.
+//        queue.add(stringRequest)
+//    }
 }
